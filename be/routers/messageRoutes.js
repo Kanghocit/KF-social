6163b68baed7c6f7d26588c62efee97f19f6a368
Kanghocit <ankhangit06@gmail.com>
2024-10-09 00:04:35 +0700

    message-API

---
+++ be/routers/messageRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import protectRoute from "../middlewares/protectRoute.js";
+import { getConversations, getMessage, sendMessage } from "../controllers/messageController.js";
+
+const router = express.Router();
+
+router.get("/:conversations", protectRoute, getConversations);
+router.get("/:otherUserId", protectRoute, getMessage);
+router.post("/", protectRoute, sendMessage);
+
+export default router;
--- be/server.js
+++ be/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './db/connectDB.js';
-import cookieParser from 'cookie-parser';
-import userRoutes from './routers/userRoutes.js'
-import postRoutes from './routers/postRoutes.js'
-import { v2 as cloudinary } from "cloudinary"
-
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routers/userRoutes.js";
+import postRoutes from "./routers/postRoutes.js";
+import messageRoutes from "./routers/messageRoutes.js";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 
@@ -17,23 +17,21 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.use(express.json({ limit: "50mb" })); // to prase  Json data in the req.body
 app.use(express.urlencoded({ extended: false })); // to prase from data in the req.body
 app.use(cookieParser());
 
-
 // routes
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
-
-
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server started at http://localhost:${PORT}`);
+  console.log(`Server started at http://localhost:${PORT}`);
 });
